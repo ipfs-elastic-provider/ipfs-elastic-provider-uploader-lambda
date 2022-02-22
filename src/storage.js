4193e5e52f125ec113b77777ce574499856a794e
Paolo Insogna <paolo@cowtech.it>
2022-02-22 12:16:54 +0100

    feat: Added telemetry. (#10)

--- src/storage.js
+++ src/storage.js
@@ -5,6 +5,7 @@ const { NodeHttpHandler } = require('@aws-sdk/node-http-handler')
 const { getSignedUrl } = require('@aws-sdk/s3-request-presigner')
 const { Agent } = require('https')
 const { logger, serializeError } = require('./logging')
+const { metrics, trackDuration } = require('./telemetry')
 
 const agent = new Agent({ keepAlive: true, keepAliveMsecs: 60000 })
 
@@ -14,7 +15,11 @@ const s3Client = new S3Client({
 
 async function isFileExisting(bucket, key) {
   try {
+    metrics.s3Heads.add(1)
+
+    const startTime = process.hrtime.bigint()
     await s3Client.send(new HeadObjectCommand({ Bucket: bucket, Key: key }))
+    trackDuration(metrics.s3HeadsDurations, startTime)
     return true
   } catch (e) {
     if (e.name !== 'NotFound') {
@@ -28,7 +33,13 @@ async function isFileExisting(bucket, key) {
 
 async function prepareUpload(bucket, key) {
   try {
-    return await getSignedUrl(s3Client, new PutObjectCommand({ Bucket: bucket, Key: key }), { expiresIn: 3600 })
+    metrics.s3Signs.add(1)
+
+    const startTime = process.hrtime.bigint()
+    const url = await getSignedUrl(s3Client, new PutObjectCommand({ Bucket: bucket, Key: key }), { expiresIn: 3600 })
+    trackDuration(metrics.s3SignsDurations, startTime)
+
+    return url
   } catch (e) {
     logger.error(`Cannot prepare file ${key} for upload in bucket ${bucket}: ${serializeError(e)}`)
     throw e
