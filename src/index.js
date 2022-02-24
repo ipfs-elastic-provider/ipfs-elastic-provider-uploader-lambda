ec1069b8c581ab781a9f84dae9d0d7b1fc5eb17f
Shogun <paolo@cowtech.it>
2022-02-24 11:15:43 +0100

    feat: Vastly improve telemetry.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 'use strict'
 
-const { setTimeout } = require('timers/promises')
 const { bucket } = require('./config')
 const { logger, elapsed, serializeError } = require('./logging')
 const { isFileExisting, prepareUpload } = require('./storage')
-const { storeMetrics } = require('./telemetry')
+const telemetry = require('./telemetry')
 
 async function main(event) {
   let key = event.path || event.rawPath // Allow integration with API Gateway or Lambda Proxy
@@ -47,11 +46,7 @@ async function main(event) {
     throw e
     /* c8 ignore next */
   } finally {
-    // Wait a little more to let all metrics being collected
-    await setTimeout(200)
-
-    // Output metrics
-    logger.info({ metrics: storeMetrics() }, 'Operation has completed.')
+    await telemetry.flush()
   }
 }
 
