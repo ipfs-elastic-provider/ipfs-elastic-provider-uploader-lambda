7cc658d6e427bc498fe0c8714289b488f608cfcc
Paolo Insogna <paolo@cowtech.it>
2022-03-03 15:52:43 +0100

    feat: Single aggregator. (#18)

--- test/miscellaneous.test.js
+++ test/miscellaneous.test.js
@@ -30,12 +30,13 @@ t.test('telemetry', async t => {
 
   // Prepare metrics
   telemetry.createMetric('custom', 'Custom', 'count')
+  telemetry.createMetric('active', 'Active', 'count')
 
   telemetry.logger = {
     info(arg) {
       t.strictSame(arg, {
         ipfs_provider_component: 'uploader-lambda',
-        metrics: { 'custom-count': 1 }
+        metrics: { 'custom-count': 1, 'active-count': -1 }
       })
     }
   }
@@ -43,6 +44,7 @@ t.test('telemetry', async t => {
   telemetry.increaseCount('custom')
   telemetry.increaseCount('custom')
   telemetry.decreaseCount('custom')
+  telemetry.decreaseCount('active')
   telemetry.flush()
 
   // Set the logger to check the refresh
@@ -50,7 +52,7 @@ t.test('telemetry', async t => {
     info(arg) {
       t.strictSame(arg, {
         ipfs_provider_component: 'uploader-lambda',
-        metrics: {}
+        metrics: { 'active-count': 0 }
       })
     }
   }
